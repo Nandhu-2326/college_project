962bdf598eb69d27ff6c6c94756ff73b5ea76334
Nandhu-2326 <kan633574@gmail.com>
2025-05-06 19:01:06 +0530

    Admin commit

--- src/JSX component/AdminUserDetails.jsx
+++ src/JSX component/AdminUserDetails.jsx
@@ -1,44 +1,202 @@
-import { FaCirclePlus } from "react-icons/fa6";
 import { LuUserRoundSearch } from "react-icons/lu";
 import { BiSolidEdit } from "react-icons/bi";
+
 import { FaUserTie } from "react-icons/fa6";
 import { MdOutlineDeleteOutline } from "react-icons/md";
-import { useNavigate } from "react-router-dom";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { useEffect, useState } from "react";
+import Swal from "sweetalert2";
+import { db } from "./Database.js";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  deleteDoc,
+  doc,
+  getDoc,
+  updateDoc,
+} from "firebase/firestore";
 
 const AdminUserDetails = () => {
-  let nav = useNavigate();
+  // State's
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [data, setData] = useState([]);
+  const [btn, setBtn] = useState(true);
+  const [currentId, setCurrentId] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [search, setSearch] = useState(false);
+  const [user, setUser] = useState();
+  // Get Users
+  const getUser = async () => {
+    const getData = await getDocs(collection(db, "Admin"));
+    const allData = getData.docs.map((val) => ({
+      id: val.id,
+      ...val.data(),
+    }));
+    setUser(allData.length);
+    setData(allData);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  // SweetAlerts
+  const InformationError = () => {
+    Swal.fire({
+      html: "Please Fill Information",
+      timer: 1000,
+      icon: "warning",
+      showConfirmButton: false,
+    });
+  };
+
+  const loading = () => {
+    Swal.fire({
+      html: "Loading...",
+      timer: 2000,
+      timerProgressBar: true,
+      didOpen: () => Swal.showLoading(),
+    });
+  };
+
+  const UserAdd = () => {
+    Swal.fire({
+      icon: "success",
+      title: "User Added",
+      timer: 2000,
+      showConfirmButton: false,
+    });
+  };
+
+  const delSuccess = () => {
+    Swal.fire({
+      icon: "success",
+      title: "User Deleted",
+      timer: 1500,
+      showConfirmButton: false,
+    });
+  };
+
+  const confirmDelete = () => {
+    return Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    });
+  };
+
+  const Update = () => {
+    Swal.fire({
+      icon: "success",
+      title: "User Updated",
+      timer: 1500,
+      showConfirmButton: false,
+    });
+  };
+
+  // Add User
+  const AddUser = async () => {
+    if (!userName || !password) {
+      InformationError();
+    } else {
+      try {
+        loading();
+        await addDoc(collection(db, "Admin"), {
+          username: userName,
+          password: password,
+        });
+        setUserName("");
+        setPassword("");
+        UserAdd();
+        getUser();
+      } catch (e) {
+        alert("Error: ", e.message);
+      }
+    }
+  };
+
+  // Delete User
+  const deleteUser = async (id) => {
+    const result = await confirmDelete();
+    if (result.isConfirmed) {
+      try {
+        await deleteDoc(doc(db, "Admin", id));
+        delSuccess();
+        getUser();
+      } catch (e) {
+        alert("Error: ", e.message);
+      }
+    }
+  };
+
+  // Edit User
+  const EditUser = async (id) => {
+    setBtn(false);
+    setCurrentId(id);
+    const getEdData = await getDoc(doc(db, "Admin", id));
+    const EdData = getEdData.data();
+    setUserName(EdData.username);
+    setPassword(EdData.password);
+  };
+
+  const UpdateUser = async () => {
+    if (!userName || !password) {
+      InformationError();
+    } else {
+      try {
+        loading();
+        await updateDoc(doc(db, "Admin", currentId), {
+          username: userName,
+          password: password,
+        });
+        setUserName("");
+        setPassword("");
+        setBtn(true);
+        setCurrentId(null);
+        Update();
+        getUser();
+      } catch (e) {
+        console.log(e.message);
+      }
+    }
+  };
+
+  // Filtered data
+  const filteredData = data.filter((user) =>
+    user.username.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <>
-      <div className="container  d-flex  justify-content-center mt-4">
-        <div className="row d-flex justify-content-center ">
-          <div className="col-12 text-primary d-flex  h1 justify-content-center align-items-center">
-            {" "}
+      <div className="container d-flex justify-content-center mt-4">
+        <div className="row d-flex justify-content-center">
+          <div className="col-12 text-primary d-flex h1 justify-content-center align-items-center">
             Admin Details - <FaUserTie />
           </div>
-          <div className="col-12 text-secondary text-center h6 mt-3 ">
-            {" "}
+          <div className="col-12 text-secondary text-center h6 mt-3">
             Search Admin or Add New Admin, Delete or Edit Admin
           </div>
 
-          <div className="col-3 mt-3 d-flex justify-content-center">
-            <button
-              className="btn  btn-outline-primary fw-semibold d-flex align-items-center justify-content-between px-3"
-              onClick={() => nav("/AdminUserPage/AdminStaff")}
-            >
-              {" "}
-              <FaCirclePlus />
-              <span>Add</span>
-            </button>
-          </div>
-          <div className="col-9 col-sm-5 mt-3">
+          <div className="col-12 col-sm-5 mt-3">
             <div className="input-group">
               <input
                 type="text"
                 className="form-control"
                 placeholder="Search User"
+                value={searchTerm}
+                onChange={(e) => {
+                  const value = e.target.value;
+                  setSearchTerm(value);
+                  setSearch(value.trim() !== "");
+                }}
               />
-              <span className="input-group-text bg-primary text-light ">
+              <span className="input-group-text bg-primary text-light">
                 <LuUserRoundSearch />
               </span>
             </div>
@@ -46,6 +204,50 @@ const AdminUserDetails = () => {
         </div>
       </div>
 
+      {/* User Form */}
+      <div className="container my-4">
+        <span className="text-primary h1 ">Total Admin - {user} </span>
+        <div className="row justify-content-center mt-5">
+          <div className="col-12 col-md-6 col-lg-5 shadow-sm p-4 bg-light rounded">
+            <div className="d-flex flex-column gap-4">
+              <div className="input-group">
+                <span className="input-group-text bg-primary text-white">
+                  <FaUserTie />
+                </span>
+                <input
+                  type="text"
+                  className="form-control border border-primary"
+                  placeholder="Admin Name"
+                  value={userName}
+                  onChange={(e) => setUserName(e.target.value)}
+                />
+              </div>
+
+              <div className="input-group">
+                <span className="input-group-text bg-primary text-white">
+                  <RiLockPasswordFill />
+                </span>
+                <input
+                  type="password"
+                  className="form-control border border-primary"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+
+              <button
+                className="btn btn-primary fw-bold py-1"
+                onClick={btn ? AddUser : UpdateUser}
+              >
+                {btn ? "Add User" : "Update Data"}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* User Table */}
       <div className="container mt-4">
         <div className="table-responsive shadow-sm">
           <table className="table table-bordered table-hover table-striped align-middle text-center">
@@ -58,40 +260,34 @@ const AdminUserDetails = () => {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>1</td>
-                <td>S.K.Ganesh Babu</td>
-                <td>
-                  <button
-                    className="btn btn-outline-danger"
-                    onClick={() => nav("/AdminUserPage/AdminStaff")}
-                  >
-                    <BiSolidEdit />
-                  </button>
-                </td>
-                <td>
-                  <button className="btn btn-outline-success">
-                    <MdOutlineDeleteOutline />
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>T.N. Dinesh Sir</td>
-                <td>
-                  <button
-                    className="btn btn-outline-danger"
-                    onClick={() => nav("/AdminUserPage/AdminStaff")}
-                  >
-                    <BiSolidEdit />
-                  </button>
-                </td>
-                <td>
-                  <button className="btn btn-outline-success">
-                    <MdOutlineDeleteOutline />
-                  </button>
-                </td>
-              </tr>
+              {(search ? filteredData : data).length > 0 ? (
+                (search ? filteredData : data).map((value, index) => (
+                  <tr key={value.id}>
+                    <td>{index + 1}</td>
+                    <td>{value.username}</td>
+                    <td>
+                      <button
+                        className="btn btn-outline-danger"
+                        onClick={() => EditUser(value.id)}
+                      >
+                        <BiSolidEdit />
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        className="btn btn-outline-success"
+                        onClick={() => deleteUser(value.id)}
+                      >
+                        <MdOutlineDeleteOutline />
+                      </button>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan="4">No Admin found</td>
+                </tr>
+              )}
             </tbody>
           </table>
         </div>
--- src/JSX component/Rmain.jsx
+++ src/JSX component/Rmain.jsx
@@ -7,7 +7,7 @@ import UserSelect from "./UserSelect";
 import SingleStudent from "./SingleStudent";
 import MultipleStudentCSV from "./MultipleStudentCSV";
 import AdminUserDetails from "./AdminUserDetails";
-import AdminStaff from "./AdminStaff";
+// import AdminStaff from "./AdminStaff";
 import CalculatorPage from "./CalculatorPage";
 import StaffAddorUpdatePage from "./StaffAddorUpdatePage";
 
@@ -19,15 +19,17 @@ const Rmain = () => {
           <Route path="/" element={<FirstPage />} />
           <Route path="/LoginPage" element={<LoginPage />} />
           <Route path="/UserSelect" element={<UserSelect />} />
-          <Route path="/CalculatorPage" element={ <CalculatorPage/> } />
+          <Route path="/CalculatorPage" element={<CalculatorPage />} />
           <Route path="/AdminLogin" element={<AdminLogin />} />
           <Route path="/AdminUserPage" element={<AdminUserPage />}>
             <Route path="SingleStudent" element={<SingleStudent />} />
-            <Route path="MultipleStudentCSV" element={ <MultipleStudentCSV/> } />
-            <Route path="AdminUserDetails" element={ <AdminUserDetails/> } />
-            <Route path="AdminStaff" element={ <AdminStaff/>} />
-            <Route path="StaffAddorUpdatePage" element = { <StaffAddorUpdatePage/> } />
-          
+            <Route path="MultipleStudentCSV" element={<MultipleStudentCSV />} />
+            <Route path="AdminUserDetails" element={<AdminUserDetails />} />
+            {/* <Route path="AdminStaff" element={ <AdminStaff/>} /> */}
+            <Route
+              path="StaffAddorUpdatePage"
+              element={<StaffAddorUpdatePage />}
+            />
           </Route>
         </Routes>
       </Router>
