3c86cc4061bd03ee1d66d5dd4c222833bb1469ad
Nandhu-2326 <kan633574@gmail.com>
2025-10-24 19:49:06 +0530

    data

--- src/JSX component/CollegeLogo.jsx
+++ src/JSX component/CollegeLogo.jsx
@@ -4,17 +4,11 @@ function CollegeLogo() {
       <div className="container-fluid shadow shadow-md ">
         <div className="row">
           <div className="col-12 d-flex justify-content-center ">
-            {/* <img
+            <img
               src="/collegeLogo.png"
               alt=""
               className="img mt-3 py-2 topLogo img-fluid "
-            /> */}
-            <h1
-              className="text-uppercase py-2 text-center"
-              style={{ color: "#0026FC" }}
-            >
-              College Internal Mark System
-            </h1>
+            />
           </div>
         </div>
       </div>
